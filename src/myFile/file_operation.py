db992089f02f0a4304134ba8790b262768585e8f
daijitao <daijitao@ctsi.com.cn>
2017-07-28 17:30:17 +0800

    add

---
+++ src/myFile/file_operation.py
@@ -0,0 +1,30 @@
+import os
+import os.path
+import shutil
+import time, datetime
+
+
+def copyFiles(sourceDir, targetDir):
+    if sourceDir.find(".svn") > 0:
+        return
+    for file in os.listdir(sourceDir):
+        sourceFile = os.path.join(sourceDir, file)
+        targetFile = os.path.join(targetDir, file)
+        if os.path.isfile(sourceFile):
+            if not os.path.exists(targetDir):
+                os.makedirs(targetDir)
+            if not os.path.exists(targetFile) or (
+                        os.path.exists(targetFile) and (os.path.getsize(targetFile) != os.path.getsize(sourceFile))):
+                open(targetFile, "wb").write(open(sourceFile, "rb").read())
+        if os.path.isdir(sourceFile):
+            First_Directory = False
+            copyFiles(sourceFile, targetFile)
+
+
+def coverFiles(sourceDir, targetDir):
+    for file in os.listdir(sourceDir):
+        sourceFile = os.path.join(sourceDir, file)
+        targetFile = os.path.join(targetDir, file)
+        # cover the files
+        if os.path.isfile(sourceFile):
+            open(targetFile, "wb").write(open(sourceFile, "rb").read())
