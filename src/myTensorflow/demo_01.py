0bf66f925ae8b0ddd073bab5035abcfbe65714d2
DaiJitao <976185561@qq.com>
2018-01-08 23:30:59 +0800

    add

--- src/myTensorflow/demo_01.py
+++ src/myTensorflow/demo_01.py
@@ -9,9 +9,21 @@ b = tf.constant([2.0,3.0], name='b')
 print(a.graph)
 print(tf.get_default_graph())
 
-g = tf.Graph()
-with g.device('/gpu:0'):
-    result = a + b
-    print(result)
 
-w1 = tf.Variable(tf.random_normal([2,3], stddev=1)) # 2行3列 标准差为1 平均值为0
+# w1 = tf.Variable(tf.random_normal([2,3], stddev=1)) # 2行3列 标准差为1 平均值为0
+state = tf.Variable(0, name="counter")
+print(state.name)
+
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+    for _ in range(3):
+        sess.run(update)
+        print(sess.run(state))
+
+print("-------------------------")
