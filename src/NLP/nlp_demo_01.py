ff42808f1aaaf254464bbbca13284446b86e59b1
DaiJitao <976185561@qq.com>
2018-06-09 08:49:49 +0800

    local

--- src/NLP/nlp_demo_01.py
+++ src/NLP/nlp_demo_01.py
@@ -1,9 +1,13 @@
 #coding:utf-8
 
 import nltk
+from nltk.text import TextCollection
 
 data = "Hello world!"
 
 tokens = nltk.word_tokenize(data)
 print(tokens)
+print("---------------------------------------")
+corpus = TextCollection(['this is sentence one','this is sentence two','this is sentence three'])
+print(corpus.tf_idf("this", "this is sentence four"))
 
---
+++ src/demo/demo_01.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+
+def add(a,b):
+    """
+
+    :param a: int
+    :param b: int
+    :return:  int
+    """
+    return a + b
