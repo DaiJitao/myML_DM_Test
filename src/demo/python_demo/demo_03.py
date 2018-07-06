a7da235f1c039f60d23a78dda11fb3c1ac59ef81
daijitao <daijitao@ctsi.com.cn>
2018-07-06 08:55:57 +0800

    变动

--- src/demo/python_demo/demo_03.py
+++ src/demo/python_demo/demo_03.py
@@ -6,3 +6,19 @@ data["name"] = "dai"
 data["grade"] = "122"
 
 print(data.get("name", None))
+
+class Person():
+    def __init__(self, name):
+        self.name = name
+
+    def test(self):
+        print("test")
+
+class EmailPerson(Person):
+    def __init__(self, name, email):
+        super().__init__(name)
+        self.email = email
+    def mm(self):
+        super().test()
+email = EmailPerson("ddai", "976@aaa")
+email.mm()
