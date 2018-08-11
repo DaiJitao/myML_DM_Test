df43c426d73eb3c581c5c535a9499edea7a6983f
DaiJitao <976185561@qq.com>
2018-08-11 23:53:49 +0800

    ww

--- src/data_mining_demo/NLP/nlp_demo_page18.py
+++ src/data_mining_demo/NLP/nlp_demo_page18.py
@@ -1,5 +1,16 @@
 # coding:utf-8
 
-import sys
-import os
+import jieba.analyse as an
 
+# an.textrank()
+# an.extract_tags()
+
+import jieba.posseg as pseg
+import jieba
+
+# jieba.enable_parallel(4)
+
+words = pseg.cut("我爱自然语言处理")
+
+for word, flag in words:
+    print(word, flag)
