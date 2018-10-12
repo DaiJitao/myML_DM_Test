702a965d983bd7bf5a59b84a2de87f18b292fb3b
DaiJitao <976185561@qq.com>
2018-10-12 23:07:16 +0800

    ww

--- src/myTensorflow/tensorflow_shizhan/chapter3.py
+++ src/myTensorflow/tensorflow_shizhan/chapter3.py
@@ -23,17 +23,29 @@ b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x , w) + b) # 预测
 # 建立损失函数
-y_ = tf.placeholder(tf.float32, (100, 10)) # 实际值
+y_ = tf.placeholder(tf.float32, (None, 10)) # 实际值
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1])) # 损失函数
 # 训练
 train_step = tf.train.GradientDescentOptimizer(.5).minimize(cross_entropy)
 
 with tf.Session() as sess:
-    init = tf.initialize_all_variables()
+    init = tf.global_variables_initializer()
     sess.run(init)
-    for i in range(1000):
-        batch_xs, batch_ys = mnist.train.next_batch(100)
-        sess.run(train_step, feed_dict={x:batch_xs, y_:batch_xs})
+    i = 0
+    while True:
+        i += 1
+        batch_xs, batch_ys = mnist.train.next_batch(60)
+        data1 = sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+        data2 = sess.run(cross_entropy, feed_dict={x: batch_xs, y_: batch_ys})
+        if i % 100 == 0:
+            print(data2)
+            if data2 < 0.07:
+                break
+    pre_w = sess.run(w)
+    print(type(pre_w))
+    print(pre_w[60])
+    print("i:",i)
+
 
 
 
