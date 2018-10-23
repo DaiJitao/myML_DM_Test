89b0680ebdc42f61a17cb58e45790be36addd206
daijitao <daijitao@ctsi.com.cn>
2018-10-23 17:23:36 +0800

    ee

--- src/data_mining_demo/machine_learning_in_action/chapter5_97.py
+++ src/data_mining_demo/machine_learning_in_action/chapter5_97.py
@@ -1,9 +1,25 @@
 
-def loadDataSet():
+import numpy as np
+
+file = r'E:\pycharm_workspace\myML_DM_Test\resource\machineLearningInAction\Ch05\testSet.txt'
+def loadDataSet(file):
     dataMat = []
     labelMat = []
-    file = 'testSet.txt'
     with open(file) as opf:
         for line in opf.readlines():
+            data = line.strip('\n').split("	")
+            tmp = [float(data[0]), float(data[1])]
+            dataMat.append(tmp)
+            labelMat.append(int(data[2]))
+    return dataMat, labelMat
 
+def sigmoid(inX):
+    return 1 / (1 + np.exp(-inX))
 
+def gradAscent(dataMatIn, classLabels):
+    """梯度下降算法"""
+    dataMatrix = np.mat(dataMatIn)
+
+dataMat, label = loadDataSet(file)
+
+print(np.array(dataMat).shape)
