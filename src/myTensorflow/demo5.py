056dbba5ae0acfe631c2e90a4f81fb00d4464f17
DaiJitao <976185561@qq.com>
2018-10-30 21:37:23 +0800

    qw

---
+++ src/myTensorflow/demo5.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=True)
+
+
+w1 = tf.Variable(tf.random_normal([2, 3], stddev=2, seed=1))
+w2 = tf.Variable(tf.random_normal([3, 1], stddev=2, seed=1))
+x = tf.constant([[.7, .9]])
+
+x_plus_w1 = tf.matmul(x, w1)
+data = tf.matmul(x_plus_w1, w2)
+
+with tf.Session(config=config) as sess:
+   a = sess.run(w1.initializer)
+   b = sess.run(w2.initializer)
+   print(a)
+   print(b)
+   print(sess.run(data))
