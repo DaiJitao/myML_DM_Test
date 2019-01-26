7c33085f93d1ca91be036aa1f46ac64c351c81ca
DaiJitao <976185561@qq.com>
2019-01-26 11:06:31 +0800

    git

--- src/ml/nlp_demo/demo2.py
+++ src/ml/nlp_demo/demo2.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 # 可视化
-import matplotlib.pyplot as mp
-mp.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
+
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文
 
 fig = plt.figure()
 plt.text(0.66, 0.41, "戴")
@@ -9,3 +9,17 @@ plt.text(0.66, 0.41, "戴")
 plt.show()
 
 import re
+
+data = "如果放到POST中将出错"
+
+import jieba
+jieba.suggest_freq(("中", "将"), True)
+seg_list = jieba.cut(data,  HMM=False)
+
+seg_list1 = jieba.cut_for_search(data)
+print("Full mode:", "/ ".join(seg_list))
+print("Full mode:", "/ ".join(seg_list1))
+result = jieba.tokenize(data)
+print(result)
+for i in result:
+    print(i)
