17a350b00d8e5a91ee87f65355cdcb1474755e56
daijitao <daijitao@ctsi.com.cn>
2019-01-29 14:51:27 +0800

    add

--- src/common/load_data.py
+++ src/common/load_data.py
@@ -2,15 +2,23 @@
 import pandas as pd
 
 class LoadData():
-    def __init__(self, path_file):
+    def __init__(self, path_file=None):
         self.file = path_file
 
-    def load_csv(self):
+    def load_csv(self, file=None):
+        if file != None:
+            self.file = file
         data = pd.read_csv(self.file, header=0)  # 指定0行为头
         return data
 
-    def load_txt(self):
-        return
+    def load_txt(self, path):
+        """ 读取文本文件 """
+        result = ""
+        with open(path, 'r', ) as tf:
+            lines = tf.readlines()
+            for line in lines:
+                result += line.strip()
+        return result
 
 
 
@@ -18,3 +26,7 @@ if __name__ == "__main__":
     file = r'D:\sys_recommand\republish_test.csv'
     load_data = LoadData(path_file = file)
     data = load_data.load_csv()
+
+    path = r"E:\pycharm_workspace\myML_DM_Test\resource\nlp_Data\C000008\10.txt"
+    content = load_data.load_txt(path)
+    print(content)
--- src/ml/nlp_demo/ltp-demo/demo1.py
+++ src/ml/nlp_demo/ltp-demo/demo1.py
@@ -1,14 +1,17 @@
-
+from common.load_data import LoadData
 from pyltp import Segmentor
+import jieba
 
 model_path = "E:/ltp3_4/cws.model"
-content = "我毕业于清华大学"
+content = "我毕业于清华大学,我朋友的名字叫戴掵莉，我哥们的名字叫付先军；阿尔艾斯是我的村庄名字"
 
 seg = Segmentor()
 seg.load(model_path) # 加载语言模型 用于分词
 words = seg.segment(content)
 seg_words = " ".join(words)
-print(" ".join(words))
+print("LTP: ", " /".join(words))
+jiebaWords = jieba.cut(content, HMM=True)
+print("jieba: ", " /".join(jiebaWords))
 print(seg_words)
 
 # 词性标注
@@ -22,3 +25,30 @@ pos_words = pos.postag(seg_words.split(" "))
 for word, pt in zip(seg_words.split(" "), pos_words):
     print(word + "/" + pt)
 
+""" 
+从分词结果上来看，对于新词的识别ltp分词方法远高于结巴分词
+"""
+print()
+print()
+text = r"E:\pycharm_workspace\myML_DM_Test\resource\nlp_Data\C000008\10.txt"
+GetData = LoadData()
+content = None or GetData.load_txt(text)
+words = seg.segment(content)
+word2s = jieba.cut(content)
+print(" /".join(words))
+print(" /".join(word2s))
+
+print("===============================================================")
+from pyltp import NamedEntityRecognizer
+# 命名实体识别
+ner_path = "E:/ltp3_4/ner.model"
+ner = NamedEntityRecognizer()
+ner.load(ner_path)
+content = " ".join(words)
+pos_words = pos.postag(content.split(" "))
+net_tags = ner.recognize(content.split(" "), pos_words)
+print(net_tags)
+result = ""
+for word, pos, name in zip(content.split(" "), pos_words, net_tags):
+    result += (word+"/"+pos+"/"+name+"   ")
+print(result)
