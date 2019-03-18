d9509379d8dfcd31b8d332f1abd23571939c47db
hejr <hejinrong@news.cn>
2019-03-18 17:12:05 +0800

    add

---
+++ src/demo/scrapy_demo/flask_demo.py
@@ -0,0 +1,21 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    return "Hello World!"
+
+@app.route("/user/<name>")
+def show_user_profile(name):
+    print("name: %s" %name)
+    return "name: %s" %name
+
+@app.route('/post/<int:id>')
+def show_post(id):
+    return "post %d" %id
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
