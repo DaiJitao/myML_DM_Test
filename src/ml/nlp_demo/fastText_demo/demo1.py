0a81079b6405f65cd9fb9571ad3f96f22e333a69
hejr <hejinrong@news.cn>
2019-05-27 10:19:51 +0800

    ‘add

--- src/ml/nlp_demo/fastText_demo/demo1.py
+++ src/ml/nlp_demo/fastText_demo/demo1.py
@@ -2,3 +2,10 @@ from gensim.models import FastText
 import gensim
 
 ldamodel = gensim.models.ldamodel.LdaModel
+
+import fasttext as ff
+
+train_data = "F:/NLP资料/fasttext/trainData/Data_train.txt"
+
+classifier = ff.skipgram(train_data, 'model')
+print(classifier.words)
